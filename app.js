af4f0ce367e7c4c433fbc9066c9ace8536c56703
paulpbrandon <108794890+paulpbrandon@users.noreply.github.com>
2022-08-01 17:43:27 +0100

    Amended to display current Auth token

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const jwt = require('jsonwebtoken');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello World, lots of love, from ${process.env.LOCATION}`);
+  res.setHeader('Content-Type', 'text/html');
+  let out = `Hello World, lots of love, from ${process.env.LOCATION}<hr/>`;
+  if (req.headers.authorization) {
+    let authHeader = req.headers.authorization;
+    out += `Raw Auth = ${authHeader} <hr/>`;
+    if (authHeader.startsWith("Bearer ")) {
+      let token = authHeader.substring(7, authHeader.length);
+      let decoded = jwt.decode(token, {complete: true});
+      if (decoded && decoded.payload) {
+        out += JSON.stringify(decoded.payload);
+      }
+    }
+  } else {
+    out += "No auth header";
+  }
+  res.end(out);
 });
 
 server.listen(port, hostname, () => {
